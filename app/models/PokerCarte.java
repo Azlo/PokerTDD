78ba144c275990cb4a79d77965303fc95148db0b
Antoine Coutellier <coutellier@info27-24.iut45.univ-orleans.fr>
2014-02-03 10:15:01 +0100

    Refactor class PokerCArte : ajout getters. tests -FAIL

--- app/models/PokerCarte.java
+++ app/models/PokerCarte.java
@@ -6,12 +6,20 @@ import play.data.validation.Constraints.*;
 
 public class PokerCarte {
 	@Required
-	public String denomination;
-	public String couleur;
+	private String denomination;
+	private String couleur;
 	public PokerCarte() {}
 	public PokerCarte(String v, String c) {
 		this.denomination = v;
 		this.couleur = c;
+	}
+
+	public String getDenomination(){
+		return this.denomination;
+	}
+
+	public String getCouleur(){
+		return this.couleur;
 	}
 
 }
