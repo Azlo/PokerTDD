95160e9ea5b006c1d60581b1d632da3d80df7120
Azlo <franck.mallart@gmail.com>
2014-02-03 09:56:26 +0100

    ajout classe PoketCarte

---
+++ app/models/PokerCarte.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.*;
+import javax.validation.*;
+import play.data.validation.Constraints.*;
+
+public class PokerCarte {
+	@Required
+	public String denomination;
+	public String couleur;
+	public PokerCarte() {}
+	public PokerCarte(String v, String c) {
+		this.denomination = v;
+		this.denomination = c;
+	}
+}
