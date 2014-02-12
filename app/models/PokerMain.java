89806e36a5b77ad7e2fccf172cc6bdfeeebee103
Coutellier Antoine <coutellier.antoine@gmail.com>
2014-02-12 10:03:00 +0100

    testNouvelleMain -SUCCESS

--- app/models/PokerMain.java
+++ app/models/PokerMain.java
@@ -4,18 +4,20 @@ import java.util.*;
 import javax.validation.*;
 import play.data.validation.Constraints.*;
 
+import models.PokerCarte;
+
 public class PokerMain {
 	@Required
-	private ArrayList<PokerCarte> main;
+	private ArrayList<PokerCarte> main = new ArrayList<PokerCarte>();
 	public PokerMain() {
-		main.add(New PokerCarte("8","carreau"));
-		main.add(New PokerCarte("7","coeur"));
-		main.add(New PokerCarte("dame","pique"));
-		main.add(New PokerCarte("roi","coeur"));
-		main.add(New PokerCarte("as","trefle"));
+		main.add(new PokerCarte("8","carreau"));
+		main.add(new PokerCarte("7","coeur"));
+		main.add(new PokerCarte("dame","pique"));
+		main.add(new PokerCarte("roi","coeur"));
+		main.add(new PokerCarte("as","trefle"));
 	}
 
-	public size() {
-		
+	public int size() {
+		return main.size();
 	}
 }
